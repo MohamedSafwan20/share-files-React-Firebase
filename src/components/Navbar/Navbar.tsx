4b793a18bc6f53f1e5527a7eb50dd6e59121fd8a
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-03-18 12:06:34 +0530

    Added dynamic navlinks

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import RouterPath from "../../config/routes";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="flex justify-around items-center p-4">
-      <div className="flex items-center gap-2">
+      <Link to={RouterPath.home} className="flex items-center gap-2">
         <img src={logo} alt="logo" className="w-[30px] h-[30px]" />
         <p className="text-[white] font-medium text-lg">Share Files</p>
-      </div>
+      </Link>
       <div className="space-x-4">
-        <Link
-          to={RouterPath.download}
-          className="text-primaryVariant uppercase hover:text-[white] text-xs"
-        >
-          Download
-        </Link>
+        {location.pathname === RouterPath.home ? (
+          <Link
+            to={RouterPath.download}
+            className="text-primaryVariant uppercase hover:text-[white] text-xs"
+          >
+            Download
+          </Link>
+        ) : location.pathname === RouterPath.download ? (
+          <Link
+            to={RouterPath.home}
+            className="text-primaryVariant uppercase hover:text-[white] text-xs"
+          >
+            Upload
+          </Link>
+        ) : (
+          <>
+            <Link
+              to={RouterPath.home}
+              className="text-primaryVariant uppercase hover:text-[white] text-xs"
+            >
+              Upload
+            </Link>
+            <Link
+              to={RouterPath.download}
+              className="text-primaryVariant uppercase hover:text-[white] text-xs"
+            >
+              Download
+            </Link>
+          </>
+        )}
+
         <Link
           to="#"
           className="text-primaryVariant uppercase hover:text-[white] text-xs"
