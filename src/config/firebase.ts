32e14a754db266e7c41b264d6bfcbabb0df82884
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-03-18 12:18:19 +0530

    Improved security

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "share-files-75e74.firebaseapp.com",
-  projectId: "share-files-75e74",
-  storageBucket: "share-files-75e74.appspot.com",
-  messagingSenderId: "932014955662",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-H4CXBGE98S",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
